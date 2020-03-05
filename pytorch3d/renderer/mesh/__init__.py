7b77dd53f4b3e407b594945cf8c22aebd40f0442
sridharsaig <53633428+sridharsaig@users.noreply.github.com>
2020-03-05 12:29:01 +0000

    Add files via upload

---
+++ pytorch3d/renderer/mesh/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+
+from .texturing import (  # isort:skip
+    interpolate_texture_map,
+    interpolate_vertex_colors,
+)
+from .rasterize_meshes import rasterize_meshes
+from .rasterizer import MeshRasterizer, RasterizationSettings
+from .renderer import MeshRenderer
+from .shader import (
+    HardFlatShader,
+    HardGouraudShader,
+    HardPhongShader,
+    SoftGouraudShader,
+    SoftPhongShader,
+    SoftSilhouetteShader,
+    TexturedSoftPhongShader,
+)
+from .shading import gouraud_shading, phong_shading
+from .utils import interpolate_face_attributes
+
+__all__ = [k for k in globals().keys() if not k.startswith("_")]
